545b54d82dff2ae393e2bd97959883ddb1f48cb9
DiegoEscobedo17 <diego.escobedo@tecsup.edu.pe>
2022-11-15 18:46:47 -0500

    Initial Commit

---
+++ ProyectoFinal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProyectoFinal
+//
+//  Created by Diego Escobedo on 11/15/22.
+//  Copyright © 2022 Diego. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
